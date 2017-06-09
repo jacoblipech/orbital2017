b5136c3c1df0279c4c59dd0a627423c4e842f796
vivekscl <vivekscl5@gmail.com>
2017-06-09 19:05:25 +0800

    Create Header.js

--- Orbital/src/components/Header.js
+++ Orbital/src/components/Header.js
@@ -1,9 +1,53 @@
 import React from 'react';
 const Header = ({message}) => {
   return(
-      <h2 className="Header text-center">
-        {message}		
-      </h2>	
+      <div className="header">
+      <div className="bg-color">
+        <header id="main-header">
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Ba<span class="logo-dec">ker</span></a>
+            </div>
+            <div className="collapse navbar-collapse" id="myNavbar">
+              <ul className="nav navbar-nav navbar-right">
+                <li className="active"><a href="#main-header">Home</a></li>
+                <li className=""><a href="#feature">About</a></li>
+                <li className=""><a href="#service">Services</a></li>
+                <li className=""><a href="#portfolio">Portfolio</a></li>
+                <li className=""><a href="#testimonial">Testimonial</a></li>
+                <li className=""><a href="#blog">Blog</a></li>
+                <li className=""><a href="#contact">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        </header>
+        <div class="wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="banner-info text-center wow fadeIn delay-05s">
+              <h1 class="bnr-title">We are at ba<span>ker</span></h1>
+              <h2 class="bnr-sub-title">Starting a new journey!!</h2>
+              <p class="bnr-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip <br>ex ea commodo consequat.</p>
+              <div class="brn-btn">
+                <a href="#" class="btn btn-download">Download now!</a>
+                <a href="#" class="btn btn-more">Learn More</a>
+              </div>
+              <div class="overlay-detail">
+                <a href="#feature"><i class="fa fa-angle-down"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
   );
   
 }; 
