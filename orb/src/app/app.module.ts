11b8a21589df9002d6ee1efee683c7dc16c31391
vivekscl <vivekscl5@gmail.com>
2017-06-13 18:58:40 +0800

    Add files via upload

--- orb/src/app/app.module.ts
+++ orb/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
+import { EditPage } from '../pages/edit/edit';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     LoginPage,
-    SignupPage
+    SignupPage,
+    EditPage
   ],
   imports: [
     BrowserModule,
@@ -25,7 +27,8 @@ import { SignupPage } from '../pages/signup/signup';
     MyApp,
     HomePage,
     LoginPage,
-    SignupPage
+    SignupPage,
+    EditPage
   ],
   providers: [
     StatusBar,
