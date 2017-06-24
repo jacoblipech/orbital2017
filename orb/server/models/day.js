e114d2b30cc87b9287a3873a81327b73bf0bfcee
Unknown <vivekscl5@gmail.com>
2017-06-24 17:28:49 +0800

    Auto stash before merge of "master" and "origin/master"
    Added days model

---
+++ orb/server/models/day.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+ 
+ //Set up the activity schema
+var DaySchema = new mongoose.Schema({
+    activity: String,
+    url: String,
+    expenses: String,
+    address: String,
+    openingHours: String,
+    nearestLandmark: String,
+    remarks: String,
+    imageUrl: String
+    
+});
+
+//exports the file to be used by other files
+module.exports = mongoose.model('Day', ActivitySchema);
